73f2379d9b85e47be584ae9b8e2a81ae65fe3d48
Modebe Stephen <modebestephen2@gmail.com>
2023-03-07 20:21:10 +0100

    Migrate to modules

---
+++ starter/catpoint-parent/image-service/src/main/java/com/udacity/imageservice/ImageService.java
@@ -0,0 +1,6 @@
+package com.udacity.imageservice;
+
+import java.awt.image.BufferedImage;
+public interface ImageService {
+    boolean imageContainsCat(BufferedImage image, float confidenceThreshhold);
+}
---
+++ starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.udacity.securityservice {
+    requires java.desktop;
+    requires miglayout;
+    requires com.udacity.imageservice;
+    requires java.prefs;
+    requires com.google.gson;
+    requires com.google.common;
+}
